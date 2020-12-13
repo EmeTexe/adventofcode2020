e2c20da64bb62d233af0b00ed83f56e1280ce10d
Poulpy <etexero56@gmail.com>
2020-12-13 12:14:29 +0100

    day 13

--- day_13/day13_shuttle_search.py
+++ day_13/day13_shuttle_search.py
@@ -7,13 +7,11 @@ with open('day13_shuttle_search.txt', 'r') as f:
             all_diff_t.append(n)
         n += 1
 
-t = all_bus[0]
-up = all_bus[0]
+t, up = all_bus[0], all_bus[0]
 for i in range(1, len(all_bus)):
     while (t + all_diff_t[i]) % all_bus[i] != 0:
         t += up
     up *= all_bus[i]
-
 print(t)
 
 # for i in range(depart_time, depart_time + min(all_bus)):
